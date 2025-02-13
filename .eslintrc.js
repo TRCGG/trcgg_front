aa53700b92fc0e61e86c6197d4215473a237047c
YerangPark <smsms_@naver.com>
2025-02-13 20:18:27 +0900

    ESLint 및 Prettier를 Airbnb 컨벤션으로 설정

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "import/prefer-default-export": "off", // Named export
+    "react/jsx-props-no-spreading": "off", // Prop spreading 허용
+    "react/require-default-props": "off", // Default props 강제 안 함
+    "react/function-component-definition": [
+      "error",
+      { namedComponents: "arrow-function" },
+    ], // 함수형 컴포넌트는 화살표 함수 사용
+    "@typescript-eslint/no-unused-vars": ["error"], // 사용하지 않는 변수 경고
+    "prettier/prettier": ["error", { endOfLine: "auto" }], // Prettier 규칙 적용
+  },
+};
