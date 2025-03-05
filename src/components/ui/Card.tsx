91c24365d868c520aeceb13c05a1da03cff7ea4d
YerangPark <smsms_@naver.com>
2025-03-05 23:22:13 +0900

    Card 공통 컴포 수정, 컬러 정의 추가, 아이콘 로드용 외부 도메인 설정 추가

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 const Card = ({ title, children, className }: CardProps) => {
   return (
     <div className={`bg-darkBg2 rounded border border-border2 ${className || ""}`}>
-      <div className="text-primary1 p-3 text-xl font-medium">{title}</div>
+      <div className="text-primary1 p-2 pl-3 text-2xl font-medium items-center">{title}</div>
       <hr className="border-t-2 border-border2" />
       <div className="flex flex-col space-y-4 p-3 text-white">{children}</div>
     </div>
