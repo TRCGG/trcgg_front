f4de341cf1da868a0b782c46f4fc6a32c9bbfdb9
YerangPark <smsms_@naver.com>
2025-04-15 20:43:19 +0900

    Nav의 '내전 전적 검색' 항목을 메인 페이지와 검색 페이지 모두에서 활성화되도록 수정
    - Figma 댓글에 알려주신 내용 적용

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   const router = useRouter();
 
   const navList = [
-    { href: "/summoners", label: "내전 전적 검색", realHref: "/" },
+    { href: "/summoners", label: "내전 전적 검색", href2: "/" },
     { href: "/tournament", label: "미니 토너먼트" },
     { href: "/team", label: "팀원 구성" },
     { href: "/community", label: "난민★그램" },
@@ -13,11 +13,12 @@ const NavBar = () => {
 
   return (
     <nav className="flex items-center space-x-4 h-full">
-      {navList.map(({ href, label, realHref }) => (
-        <Link key={realHref || href} href={realHref || href}>
+      {navList.map(({ href, label, href2 }) => (
+        <Link key={href2 || href} href={href2 || href}>
           <span
             className={`cursor-pointer text-base font-medium pb-1 whitespace-nowrap ${
               router.pathname === href ||
+              router.pathname === href2 ||
               (href === "/summoners" && router.pathname.startsWith("/summoners"))
                 ? "text-primary1 border-b-3 border-primary1"
                 : "text-primary2 border-b-3 border-transparent"
