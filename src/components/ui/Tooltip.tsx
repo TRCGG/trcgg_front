4f7a78bb68da73fec2ed7d131f380c5053d508c2
YerangPark <smsms_@naver.com>
2026-01-20 17:52:35 +0900

    툴팁 버그 수정

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -17,11 +17,11 @@ const Tooltip = ({ content, children, className = "" }: Props) => {
     >
       {children}
       {isVisible && content && (
-        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 z-50 pointer-events-none">
-          <div className="px-3 py-2 rounded bg-black text-white text-sm max-w-[320px] break-words overflow-wrap-anywhere">
+        <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 z-[9999] pointer-events-none">
+          <div className="px-3 py-2 rounded bg-black text-white text-sm w-[240px] max-w-[320px] whitespace-normal break-words text-left">
             {content}
           </div>
-          <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-black" />
+          <div className="absolute top-full left-1/2 -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-black" />
         </div>
       )}
     </div>
